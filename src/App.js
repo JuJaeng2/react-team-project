b094fd24a6f1df18754dee6e3106c13ceb14ceb3
wsmKR <150803290+wsmKR@users.noreply.github.com>
2023-11-26 19:35:48 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import MainPage from './MainPage/MainPage';
 import Map from './MainPage/Map';
+import Login from './Login';
+import MyPage from './MyPage';
 
 function App() {
   return (
