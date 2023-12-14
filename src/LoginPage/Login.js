097be247b477ba65eaea427e6b69ff8aca92d833
wsmKR <wsm2695@naver.com>
2023-12-14 15:53:22 +0900

    Community, Login Modify 12/14
    Community, Login Modify

--- src/LoginPage/Login.js
+++ src/LoginPage/Login.js
@@ -1,68 +1,71 @@
-import React, { Component } from 'react';
-// import Input from './Input01';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '홍길동',
-      password: '',
-      userList: [
-        { name: '홍길동', pass: '1234' },
-        { name: '이몽룡', pass: '2345' },
-        { name: '성춘향', pass: '4567' },
-      ],
-    };
-    this.onChange = this.onChange.bind(this);
-    this.onLoginHandler = this.onLoginHandler.bind(this);
-  }
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  onChange(key, value) {
-    this.setState({
-      [key]: value,
-    });
-  }
+  const handleLogin = () => {
+    const hardcodedUsername = "exampleUser";
+    const hardcodedPassword = "examplePassword";
 
-  onLoginHandler() {
-    const { loginHandler } = this.props;
-    const { name, password, userList } = this.state;
-    userList.forEach((userInfo) => {
-      if (name === userInfo.name && password === userInfo.pass) {
-        loginHandler();
-      }
-    });
-  }
+    if (username === hardcodedUsername && password === hardcodedPassword) {
+      console.log("로그인 성공");
+    } else {
+      setError("유효하지 않은 사용자명 또는 비밀번호");
+    }
+  };
 
-  render() {
-    return (
+  return (
+    <div>
+      <nav>
+        <div className="nav-wrapper brown lighten-2">
+          <Link to="/">
+            <span className="brand-logo center">멍멍 왈왈</span>
+          </Link>
+          <ul id="nav-mobile" className="left hide-on-med-and-down">
+            <li>
+              <Link to="/local">보호소 찾기</Link>
+            </li>
+            <li>
+              <Link to="/dog-page">품종별 조회</Link>
+            </li>
+            <li>
+              <Link to="/login">로그인 / 회원가입</Link>
+            </li>
+            <li>
+              <Link to="/board">커뮤니티</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
       <div>
-        {/* <div>
-          <Input
-            label="이름"
-            name="name"
-            value={this.state.name}
+        <h2>로그인</h2>
+        <div>
+          <label htmlFor="username">사용자명:</label>
+          <input
             type="text"
-            errorMessage="이름을 입력해야 합니다"
-            autoFocus
-            onChange={this.onChange}
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div>
-          <Input
-            label="비밀번호"
-            name="password"
-            value={this.state.password}
+          <label htmlFor="password">비밀번호:</label>
+          <input
             type="password"
-            errorMessage="비밀번호를 입력해야 합니다"
-            onChange={this.onChange}
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-        </div> */}
-        <div>
-          <button onClick={this.onLoginHandler}>Login</button>
         </div>
+        <button onClick={handleLogin}>로그인</button>
+        {error && <p style={{ color: "red" }}>{error}</p>}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Login;
