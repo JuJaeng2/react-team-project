672b923b7914908717c2dc54ceb16eeabb58066a
wsmKR <wsm2695@naver.com>
2023-12-14 10:37:23 +0900

    board modify
    board modify

--- src/CommunityPage/Board.js
+++ src/CommunityPage/Board.js
@@ -1,57 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Board = () => {
+  const [postText, setPostText] = useState("");
   const [posts, setPosts] = useState([]);
-  const [newPost, setNewPost] = useState({ title: "", content: "" });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setNewPost({ ...newPost, [name]: value });
-  };
+  useEffect(() => {
+    const storedPosts = JSON.parse(localStorage.getItem("posts")) || [];
+    setPosts(storedPosts);
+  }, []);
 
   const handlePostSubmit = () => {
-    // 신규 게시글 추가
-    setPosts([...posts, newPost]);
-    setNewPost({ title: "", content: "" });
+    const newPost = { id: Date.now(), text: postText };
+    const updatedPosts = [...posts, newPost];
+    localStorage.setItem("posts", JSON.stringify(updatedPosts));
+
+    setPostText("");
+    setPosts(updatedPosts);
   };
 
   return (
     <div>
-      <h1>게시판</h1>
+      <nav>
+        <div className="nav-wrapper brown lighten-2">
+          <Link to="/">
+            <span className="brand-logo center">멍멍 왈왈</span>
+          </Link>
+          <ul id="nav-mobile" className="left hide-on-med-and-down">
+            <li>
+              <Link to="/local">보호소 찾기</Link>
+            </li>
+            <li>
+              <Link to="/dog-page">품종별 조회</Link>
+            </li>
+            <li>
+              <Link to="/login">로그인 / 회원가입</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-      {/* 새로운 게시글 작성 폼 */}
-      <form>
-        <label>
-          제목:
-          <input
-            type="text"
-            name="title"
-            value={newPost.title}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          내용:
-          <textarea
-            name="content"
-            value={newPost.content}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <button type="button" onClick={handlePostSubmit}>
-          게시글 작성
-        </button>
-      </form>
-
-      {/* 게시글 목록 출력 */}
+      <h2>게시물 작성</h2>
+      <textarea
+        rows="4"
+        cols="50"
+        placeholder="게시글을 작성하세요..."
+        value={postText}
+        onChange={(e) => setPostText(e.target.value)}
+      ></textarea>
+      <br />
+      <button onClick={handlePostSubmit}>Post</button>
+      <hr />
+      <h2>나의 게시물</h2>
       <ul>
-        {posts.map((post, index) => (
-          <li key={index}>
-            <strong>{post.title}</strong>
-            <p>{post.content}</p>
-          </li>
+        {posts.map((post) => (
+          <li key={post.id}>{post.text}</li>
         ))}
       </ul>
     </div>
